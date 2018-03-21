344675bcfe49e973d33a74d6f65669f79552928e
blondeaa <antoine.blondeau@etudiant.univ-lille1.fr>
2018-03-21 15:36:31 +0100

    init2

---
+++ app/src/main/java/com/example/blondeaa/taverneoubliee/TaverneOubliee.java
@@ -0,0 +1,13 @@
+package com.example.blondeaa.taverneoubliee;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TaverneOubliee extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_taverne_oubliee);
+    }
+}
