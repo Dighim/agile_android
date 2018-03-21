aa35afafee5c3b97b6ef17b89cda97d1abc72243
Antoine BLONDEAU <blondeaa@epicea11.iut-infobio.priv.univ-lille1.fr>
2018-03-21 10:21:51 +0100

    init commit

---
+++ app/src/main/java/fr/univ_lille1/iutinfo/taverneoubliee/TaverneOubliee.java
@@ -0,0 +1,15 @@
+package fr.univ_lille1.iutinfo.taverneoubliee;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TaverneOubliee extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_taverne_oubliee);
+    }
+
+
+}
